e09812fa571083398937f47255db05ead18471e3
Louis_0922 <a9603s9603@gmail.com>
2024-07-06 12:59:12 +0800

    new project

---
+++ vote_system-backend/vote_system/Program.cs
@@ -0,0 +1,12 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+app.Run();
